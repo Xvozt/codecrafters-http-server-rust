94b4d60d42ca0f395d5b469de4158821827112a4
s.v.kupriyanov <s.v.kupriyanov@tinkoff.ru>
2026-01-09 16:35:34 +0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,62 @@
-use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+enum HttpMethod {
+    GET,
+    POST,
+}
+struct HttpRequest {
+    method: HttpMethod,
+    uri: String,
+}
+
+impl HttpRequest {
+    fn from_tcp_stream(stream: &mut TcpStream) -> Result<Option<Self>, String> {
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let bytes = match stream.read(&mut buffer) {
+            Ok(bytes) => bytes,
+            Err(e) => return Err(format!("Error reading stream: {}", e)),
+        };
+
+        if bytes == 0 {
+            return Ok(None);
+        }
+
+        let request_str = String::from_utf8_lossy(&buffer);
+
+        let mut lines = request_str.lines();
+
+        if let Some(request_line) = lines.next() {
+            let parts: Vec<&str> = request_line.split_whitespace().collect();
+            if parts.len() >= 2 {
+                let method = match parts[0] {
+                    "GET" => HttpMethod::GET,
+                    "POST" => HttpMethod::POST,
+                    _ => return Err("Unsuported method".to_string()),
+                };
+                let uri = parts[1].to_string();
+                return Ok(Some(HttpRequest { method, uri: uri }));
+            }
+        }
+        Ok(None)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    if let Ok(Some(request)) = HttpRequest::from_tcp_stream(&mut stream) {
+        match request.uri.as_str() {
+            "/" => stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap(),
+            _ => stream
+                .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                .unwrap(),
+        };
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,9 +68,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
